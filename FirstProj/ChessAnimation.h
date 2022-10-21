2ad08eb8c75123b0426e7202b6222af6bceb3509
myleslouw <56117169+myleslouw@users.noreply.github.com>
2022-10-21 14:49:15 +0200

    added chessAnimation class

---
+++ FirstProj/ChessAnimation.h
@@ -0,0 +1,48 @@
+#pragma once
+//GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <GL/glew.h>
+
+#include "AnimationManager.h"
+
+class ChessAnimation
+{
+	//this class controls the animation of the chess pieces
+
+public:
+
+	ChessAnimation();
+
+	//array of the piecs positions
+	glm::vec3 chessPieces[8] = {
+		{ glm::vec3(-7, 1, 7) },
+		{ glm::vec3(-5, 1, 5) },
+		{ glm::vec3(-3, 1, 7) },
+		{ glm::vec3(-1, 1, 5) },
+		{ glm::vec3(1, 1, 5) },
+		{ glm::vec3(3, 1, 7) },
+		{ glm::vec3(5, 1, 5) },
+		{ glm::vec3(7, 1, 7) },
+	};
+
+
+	glm::vec3 testPiece;
+
+	void MovePiece(GLfloat deltaTime);
+
+	void AnimatePiece(int index, GLfloat deltaTime);
+
+
+	~ChessAnimation();
+
+private:
+
+	int speedDirection;
+	int maxY, minY;
+	bool pieceCompleted = false;
+
+
+};
+
